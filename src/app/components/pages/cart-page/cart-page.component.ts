f65dbe21107f12302205eb345a53f45bd886ab15
Stefan Nikic <stefannikic15@gmail.com>
2025-05-21 03:05:16 +0200

    Zavrseno stilizovanje Cart Page-a (HTML & CSS); kao i dodavanje importa u TS fajl

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -3,10 +3,12 @@ import { CartService } from '../../../services/cart.service';
 import { Cart } from '../../../shared/models/Cart';
 import { CartItem } from '../../../shared/models/CartItem';
 import { TitleComponent } from "../../partials/title/title.component";
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [TitleComponent],
+  imports: [TitleComponent, RouterLink, NgIf, NgFor, CurrencyPipe],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
